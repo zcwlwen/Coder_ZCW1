0c5c17cda63b94b376800cbab835f96caf21f951
zcwlwen <zcw_lwen@sina.cn>
2016-07-07 14:33:57 +0800

    Initial Commit

---
+++ Coder_ZCW1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Coder_ZCW1
+//
+//  Created by 张朝伟 on 16/7/7.
+//  Copyright © 2016年 zcwlwen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Coder_ZCW1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Coder_ZCW1
+//
+//  Created by 张朝伟 on 16/7/7.
+//  Copyright © 2016年 zcwlwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
